a5fce63bd401aa3887fa7026ee2da728782cdbd3
Anthony Fernandez <anthony.fernandez2021@campus-eni.fr>
2021-10-28 15:43:25 +0200

    ADD IHM Ajout potager

--- src/main/java/fr/anthomane/potager/bo/Potager.java
+++ src/main/java/fr/anthomane/potager/bo/Potager.java
@@ -7,6 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
@@ -25,9 +28,13 @@ public class Potager {
 	@Id
 	@GeneratedValue
 	private Integer idPotager;
+	@NotBlank
 	private String localisation;
+	@NotBlank
 	private String nom;
+	@NotNull
 	private Double surface;
+	@NotBlank
 	private String ville;
 	
 	@OneToMany(mappedBy = "potager")
--- src/main/java/fr/anthomane/potager/ihm/PotagerController.java
+++ src/main/java/fr/anthomane/potager/ihm/PotagerController.java
@@ -1,12 +1,17 @@
 package fr.anthomane.potager.ihm;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import fr.anthomane.potager.bll.PotagerManager;
+import fr.anthomane.potager.bo.Potager;
 
 @Controller
 public class PotagerController {
@@ -26,22 +31,32 @@ public class PotagerController {
 		return "potagerList";
 	}
 	
+	@GetMapping("potager/add")
+	public String potagerAdd(Potager p, Model model) {
+		return "potagerAdd";
+	}
+	
+	@PostMapping("potager/add")
+	public String potagerSubmit(@Valid Potager p, BindingResult errors, Model model) {
+		 if (errors.hasErrors()) {
+			 return "potagerAdd";
+		 }else {
+			 manager.addPotager(p);
+			 model.addAttribute("message","Potager " + p.getNom() + " ajouté avec succès");
+			 return "redirect:/potager/liste";
+		 }
+	}
+	
 	@GetMapping("potager/{id}")
 	public String potagerDetail(@PathVariable("id") Integer id,Model model) {
 		model.addAttribute("potager", manager.getPotagerById(id));
 		return "potagerDetail";
 	}
-
+	
 	@GetMapping("carre/{id}")
 	public String carreDetail(@PathVariable("id") Integer id,Model model) {
 		model.addAttribute("carre", manager.getCarreById(id));
 		return "carreDetail";
-	}
-	
-	@GetMapping("plante/{id}")
-	public String planteDetail(@PathVariable("id") Integer id,Model model) {
-		model.addAttribute("plante", manager.getPlanteById(id));
-		return "planteDetail";
 	}
 	
 	@GetMapping("plante/liste")
@@ -49,4 +64,16 @@ public class PotagerController {
 		model.addAttribute("plantes", manager.getAllPlantes());
 		return "planteList";
 	}
+	
+	@GetMapping("plante/{id}")
+	public String planteDetail(@PathVariable("id") Integer id,Model model) {
+		model.addAttribute("plante", manager.getPlanteById(id));
+		return "planteDetail";
+	}
+
+	
+	
+	
+	
+	
 }
