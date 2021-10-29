eff69828dcfe43852ffc0f65d1e7880931782f69
Anthony Fernandez <anthony.fernandez2021@campus-eni.fr>
2021-10-29 11:37:49 +0200

    Add add functions before shutdown

--- src/main/java/fr/anthomane/potager/ihm/PotagerController.java
+++ src/main/java/fr/anthomane/potager/ihm/PotagerController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import fr.anthomane.potager.bll.PotagerManager;
 import fr.anthomane.potager.bll.PotagerManagerException;
 import fr.anthomane.potager.bo.Carre;
+import fr.anthomane.potager.bo.Implantation;
+import fr.anthomane.potager.bo.Plante;
 import fr.anthomane.potager.bo.Potager;
 
 @Controller
@@ -30,18 +32,18 @@ public class PotagerController {
 	@GetMapping("potager/liste")
 	public String potagerListe(Model model) {
 		model.addAttribute("potagers", manager.getAllPotagers());
-		return "potagerList";
+		return "potager/potagerList";
 	}
 	
 	@GetMapping("potager/add")
 	public String potagerAdd(Potager p, Model model) {
-		return "potagerAdd";
+		return "potager/potagerAdd";
 	}
 	
 	@PostMapping("potager/add")
 	public String potagerSubmit(@Valid Potager p, BindingResult result, Model model) {
 		 if (result.hasErrors()) {
-			 return "potagerAdd";
+			 return "potager/potagerAdd";
 		 }else {
 			 manager.addPotager(p);
 			 model.addAttribute("message","Potager " + p.getNom() + " ajouté avec succès");
@@ -52,7 +54,7 @@ public class PotagerController {
 	@GetMapping("potager/{id}")
 	public String potagerDetail(@PathVariable("id") Integer id,Model model) {
 		model.addAttribute("potager", manager.getPotagerById(id));
-		return "potagerDetail";
+		return "potager/potagerDetail";
 	}
 	
 	@GetMapping("potager/{id}/add")
@@ -60,14 +62,19 @@ public class PotagerController {
 		Potager p = manager.getPotagerById(id);
 		c.setPotager(p);
 		model.addAttribute("potager", p);
-		return "carreAdd";
+		return "carre/carreAdd";
 	}
-	
+	/**
+	 * Traite le formulaire d'ajout d'un carré
+	 * @param c le carré généré par le formulaire
+	 * @param result BindingResult
+	 * @param model
+	 * @return
+	 */
 	@PostMapping("carre/add")
 	public String carreSubmit(@Valid Carre c, BindingResult result, Model model) {
-		System.err.println(c.getPotager());
 		 if (result.hasErrors()) {
-			 return "carreAdd";
+			 return "carre/carreAdd";
 		 }else {
 			 try {
 				manager.addCarre(c);
@@ -75,26 +82,74 @@ public class PotagerController {
 			} catch (PotagerManagerException e) {
 				e.printStackTrace();
 			}
-			 return "redirect:/potager/liste";
+			 return "redirect:/potager/" + c.getPotager().getIdPotager();
 		 }
 	}
 	
 	@GetMapping("carre/{id}")
 	public String carreDetail(@PathVariable("id") Integer id,Model model) {
 		model.addAttribute("carre", manager.getCarreById(id));
-		return "carreDetail";
+		return "carre/carreDetail";
 	}
+	
+	@GetMapping("carre/{id}/add")
+	public String implantationAdd(@PathVariable("id") Integer id, Implantation i, Model model) {
+		Carre c = manager.getCarreById(id);
+		i.setCarre(c);
+		model.addAttribute("carre", c);
+		model.addAttribute("plantes", manager.getAllPlantes());
+		return "implantation/implantationAdd";
+	}
+	
+	@PostMapping("implantation/add")
+	public String implantationSubmit(@Valid Implantation i, BindingResult result, Model model) {
+		System.err.println(result); 
+		if (result.hasErrors()) {
+			 model.addAttribute("carre", i.getCarre());
+			 model.addAttribute("plantes", manager.getAllPlantes());
+			 return "implantation/implantationAdd";
+		 }else {
+			 try {
+				manager.addImplantation(i);
+				model.addAttribute("message","Implantation ajoutée avec succès");
+			} catch (PotagerManagerException e) {
+				e.printStackTrace();
+			}
+			 return "redirect:/carre/" + i.getCarre().getIdCarre();
+		 }
+	}
+	
 	
 	@GetMapping("plante/liste")
 	public String planteListe(Model model) {
 		model.addAttribute("plantes", manager.getAllPlantes());
-		return "planteList";
+		return "plante/planteList";
 	}
 	
 	@GetMapping("plante/{id}")
 	public String planteDetail(@PathVariable("id") Integer id,Model model) {
 		model.addAttribute("plante", manager.getPlanteById(id));
-		return "planteDetail";
+		return "plante/planteDetail";
+	}
+	
+	@GetMapping("plante/add")
+	public String planteAdd(Plante p, Model model) {
+		return "plante/planteAdd";
+	}
+	
+	@PostMapping("plante/add")
+	public String planteSubmit(@Valid Plante p, BindingResult result, Model model) {
+		 if (result.hasErrors()) {
+			 return "plante/planteAdd";
+		 }else {
+			 try {
+				manager.addPlante(p);
+				model.addAttribute("message","Plante " + p.getNom() + " ajouté avec succès");
+			} catch (PotagerManagerException e) {
+				e.printStackTrace();
+			}
+			 return "redirect:/plante/liste";
+		 }
 	}
 
 	
